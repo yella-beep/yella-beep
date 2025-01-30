ba0bfa083a59649229d678cfb3fc89118433241a
yella-beep <yellareddy939@gmail.com>
2025-01-30 15:29:34 +0530

    Add files via upload

---
+++ start.js
@@ -0,0 +1,8 @@
+let arr = [1, 8, 8, 5, 9, 5, 4];  
+let sum = 0; // Initialize sum
+
+for (let i = 0; i < arr.length; i++) {  
+    sum += arr[i]; // Add each number to sum
+}
+
+console.log("Sum of all numbers:", sum);
